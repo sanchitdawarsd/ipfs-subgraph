7da67dbac25bf1f2da81471dde3b66de3bd404c9
--replace-all <sanchitdawar22@gmail.com>
2025-02-02 18:52:09 +0530

    fix: string and bytes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,16 +1,14 @@
 import { ByteArray, Bytes, DataSourceContext, DataSourceTemplate, dataSource, ipfs, json, log, ethereum, bigInt } from '@graphprotocol/graph-ts'
 import { TokenURIModified, TokenMinted } from '../generated/CorpData/CorpData'
 import { EmployeeData, PostContent } from '../generated/schema'
-import { POST_ID_KEY, POST_ID_KEY_UPDATED, addQm } from './helper'
+import { POST_ID_KEY } from './helper'
 
-function handleIPFSContent(uri: string, entityId: Bytes, isUpdate: boolean, block: ethereum.Block): void {
+function handleIPFSContent(uri: string, entityId: Bytes): void {
   let ipfsIndex = uri.indexOf('/ipfs/');
   if (ipfsIndex == -1) return;
 
   let context = new DataSourceContext();
-  context.setBytes(isUpdate ? POST_ID_KEY_UPDATED : POST_ID_KEY, entityId);
-  context.setString('blockNumber', block.number.toString());
-  context.setString('timeStamp', block.timestamp.toString());
+  context.setBytes(POST_ID_KEY, entityId);
   
   let hash = uri.substr(ipfsIndex + 6);
   DataSourceTemplate.createWithContext('IpfsContent', [hash], context);
@@ -23,7 +21,7 @@ export function handleTokenMinted(event: TokenMinted): void {
   data.uri = event.params.uri
   data.save()
   
-  handleIPFSContent(data.uri, data.id, false, event.block)
+  handleIPFSContent(data.uri, data.id)
 }
 
 export function handleTokenURIModified(event: TokenURIModified): void {
@@ -37,46 +35,29 @@ export function handleTokenURIModified(event: TokenURIModified): void {
   data.uri = event.params.newUri
   data.save()
   
-  handleIPFSContent(data.uri, data.id, true, event.block)
+  handleIPFSContent(data.uri, data.id)
 }
 
 export function handlePostContent(content: Bytes): void {
   let hash = dataSource.stringParam();
   let ctx = dataSource.context();
-  let blockNumber = (ctx.getString('blockNumber'));
-  let timeStamp = (ctx.getString('timeStamp'));
   
-  // Try to load with updated key first
-  let id = ctx.getBytes(POST_ID_KEY_UPDATED);
-  if (id.length == 0) {
-    id = ctx.getBytes(POST_ID_KEY);
-  }
+  let id = ctx.getBytes(POST_ID_KEY);
   
-  // Create unique ID for this version of the content
-  let versionedId = Bytes.fromUTF8(id.toHexString() + '-' + blockNumber.toString());
-  
-  let post = PostContent.load(versionedId);
+  let post = PostContent.load(id);
   if (!post) {
-    post = new PostContent(versionedId);
-    post.originalId = id;
-    post.version = 0;
-  } else {
-    post.version = post.version + 1;
+    post = new PostContent(id);
   }
   
   log.info(
-    "Processing IPFS content - Hash: {}, ID: {}, Block: {}, Version: {}", 
+    "Processing IPFS content - Hash: {}, ID: {}", 
     [
       hash, 
       id.toHexString(),
-      blockNumber.toString(),
-      post.version.toString()
     ]
   );
   
   post.hash = hash;
   post.content = content.toString();
-  post.blockNumber = blockNumber;
-  post.timestamp = timeStamp;
   post.save();
 }
